7cfdb49b567554009e0f9604152ac02f842a4c19
Yusba <yusba50@gmail.com>
2023-01-30 16:29:00 +0100

    first commit

---
+++ Index.py
@@ -0,0 +1,53 @@
+# Go to https://replit.com/@appbrewery/rock-paper-scissors-start?v=1
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+game_images = [rock,paper,scissors]
+user_choice =int(input("What did you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice > 2 or user_choice < 0:
+    print("You typed an invalid number,You Lose")
+else:
+    print(game_images[user_choice])
+computer_choice =random.randint(0,2)
+print(game_images[computer_choice])
+if user_choice > 2 or user_choice < 0:
+    print("You typed an invalid number,You Lose")
+elif user_choice==0 and computer_choice==2:
+    print("You win!")
+elif computer_choice==0 and user_choice==2:
+    print("You lose")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice==user_choice:
+    print("Its a Draw")
+
+
